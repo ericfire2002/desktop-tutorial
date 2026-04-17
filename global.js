3aca000f9b09d9e53418d9eb865222db139dd11b
Eric <emei@ucsd.edu>
2026-04-16 18:03:05 -0700

    hi

--- global.js
+++ global.js
@@ -3,12 +3,51 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
-let navLinks = $$("nav a");
+// let navLinks = $$("nav a");
 
-// Step 2.2: find the current page link
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+// // Step 2.2: find the current page link
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname
+// );
 
-// Step 2.3: add the class safely
-currentLink?.classList.add('current');
+// // Step 2.3: add the class safely
+// currentLink?.classList.add('current');
+
+const BASE_PATH =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    ? '/'
+    : '/desktop-tutorial/';
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/ericfire2002', title: 'GitHub' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname,
+  );
+
+  a.toggleAttribute('target', a.host !== location.host);
+  if (a.host !== location.host) {
+    a.rel = 'noopener noreferrer';
+  }
+
+  nav.append(a);
+}
