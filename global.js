49ecd6a9adfcee056a6cfab2b472fef23ef2e050
Eric <emei@ucsd.edu>
2026-04-16 17:53:20 -0700

    x

--- global.js
+++ global.js
@@ -3,3 +3,12 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+let navLinks = $$("nav a");
+
+// Step 2.2: find the current page link
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+// Step 2.3: add the class safely
+currentLink?.classList.add('current');
